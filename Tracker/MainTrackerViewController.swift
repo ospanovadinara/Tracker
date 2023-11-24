1b8f329d90445c8002ac9f2c5c1708e2e5de94a4
ospanovadinara <ospan.dinara@yandex.ru>
2023-11-24 21:07:43 +0600

    добавила SnapKit, настроила NavigationBar

--- Tracker/MainTrackerViewController.swift
+++ Tracker/MainTrackerViewController.swift
@@ -6,11 +6,54 @@
 //
 
 import UIKit
+import SnapKit
 
-class MainTrackerViewController: UIViewController {
+final class MainTrackerViewController: UIViewController {
+
+    // MARK: - UI
+    private lazy var navBarTitle: UILabel = {
+        let title = UILabel()
+        title.text = "Трекеры"
+        title.font = UIFont.boldSystemFont(ofSize: 34)
+        title.textColor = UIColor.black
+        title.sizeToFit()
+        return title
+    }()
+
+    private lazy var navBarButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(image: UIImage(named: "plus_icon"),
+                                     style: .plain,
+                                     target: self,
+                                     action: #selector(navBarButtonTapped))
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+        setupViews()
+        setupConstraints()
+    }
+
+    private func setupNavigationBar() {
+        navigationItem.leftBarButtonItem = navBarButton
+        let navigationController = UINavigationController(rootViewController: MainTrackerViewController())
+    }
+
+    private func setupViews() {
+        view.addSubview(navBarTitle)
+    }
+
+    private func setupConstraints() {
+        navBarTitle.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(88)
+            make.leading.equalToSuperview().offset(16)
+            make.height.equalTo(41)
+        }
+    }
+
+    // MARK: - Actions
+    @objc private func navBarButtonTapped() {
 
     }
 }
