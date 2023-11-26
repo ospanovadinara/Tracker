9ea4ab32c904289e86b316b43c0471abe39fff54
ospanovadinara <ospan.dinara@yandex.ru>
2023-11-26 20:26:43 +0600

    добавлена ячейка для трекеров

---
+++ Tracker/Tracker/TrackersCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrackersCell.swift
+//  Tracker
+//
+//  Created by Dinara on 26.11.2023.
+//
+
+import UIKit
+
+final class TrackersCell: UICollectionViewCell {
+    // MARK: - Public properties
+    public static let cellID = String(describing: TrackersCell.self)
+}
