97cb2fedbbb9f0ceb291bbc0b553a0a7fc7933e2
ospanovadinara <ospan.dinara@yandex.ru>
2024-03-13 20:49:14 +0500

    TrackerRecordStore fixed

--- Tracker/Domain/TrackerRecordStore.swift
+++ Tracker/Domain/TrackerRecordStore.swift
@@ -8,17 +8,73 @@
 import Foundation
 import CoreData
 
-final class TrackerRecordStore {
+enum TrackerRecordStoreError: Error {
+    case decodingErrorInvalidTitle
+}
+
+struct TrackerRecordStoreUpdate {
+    struct Move: Hashable {
+        let oldIndex: Int
+        let newIndex: Int
+    }
+    let insertedIndexes: IndexSet
+    let deletedIndexes: IndexSet
+    let updatedIndexes: IndexSet
+    let movedIndexes: Set<Move>
+}
+
+protocol TrackerRecordStoreDelegate: AnyObject {
+    func store(_ store: TrackerRecordStore, didUpdate update: TrackerRecordStoreUpdate)
+}
+
+final class TrackerRecordStore: NSObject {
     static let shared = TrackerRecordStore()
     private let context: NSManagedObjectContext
+    private var fetchedResultsController: NSFetchedResultsController<TrackerRecordCoreData>!
+    weak var delegate: TrackerRecordStoreDelegate?
 
-    convenience init() {
+    private var insertedIndexes: IndexSet?
+    private var deletedIndexes: IndexSet?
+    private var updatedIndexes: IndexSet?
+    private var movedIndexes: Set<TrackerRecordStoreUpdate.Move>?
+
+    var trackerRecords: [TrackerRecord] {
+        guard let objects = self.fetchedResultsController.fetchedObjects,
+              let records = try? objects.map ({
+                  try self.trackerRecord(from: $0)
+              })
+        else { return [] }
+
+        return records
+    }
+
+    convenience override init() {
         let context = DataManager.shared.context
         self.init(context: context)
+
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            assertionFailure("TrackerRecordStore fetch failed")
+        }
     }
 
     init(context: NSManagedObjectContext) {
         self.context = context
+        super.init()
+
+        let fetchRequest = TrackerRecordCoreData.fetchRequest()
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(keyPath: \TrackerRecordCoreData.date, ascending: true)
+        ]
+        let controller = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: context,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+        controller.delegate = self
+        self.fetchedResultsController = controller
     }
 
     func addNewTracker(_ trackerRecord: TrackerRecord) throws {
@@ -30,13 +86,38 @@ final class TrackerRecordStore {
         try context.save()
     }
 
-    func deleteTrackerRecord(_ trackerRecord: TrackerRecord) throws {
-        let request = NSFetchRequest<TrackerRecordCoreData>(entityName: "TrackerRecordCoreData")
-        request.predicate = NSPredicate(format: "id == %@ AND date == %@", trackerRecord.trackerID as CVarArg, trackerRecord.date as CVarArg)
-
-        if let existingRecord = try context.fetch(request).first {
-            context.delete(existingRecord)
+    func deleteTrackerRecord(with id: UUID) throws {
+        let request = TrackerRecordCoreData.fetchRequest()
+        let trackerRecordFromCoreData = try context.fetch(request)
+        let record = trackerRecordFromCoreData.first {
+            $0.id == id
+        }
+        if let record = record {
+            context.delete(record)
             try context.save()
+        }
+        reload()
+    }
+
+    func deleteAllTrackerRecords(with trackerID: UUID) throws {
+        let request = NSFetchRequest<TrackerRecordCoreData>(entityName: "TrackerRecordCoreData")
+        request.predicate = NSPredicate(format: "id == %@", trackerID as CVarArg)
+        do {
+            let allRecords = try context.fetch(request)
+            for record in allRecords {
+                context.delete(record)
+            }
+            try context.save()
+        } catch {
+            throw error
+        }
+    }
+
+    func reload() {
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("Не удалось обновить данные в хранилище: \(error)")
         }
     }
 
@@ -51,11 +132,55 @@ final class TrackerRecordStore {
             trackerID: id
         )
     }
+}
 
-    func fetchTrackerRecords() throws -> [TrackerRecord] {
-        let request = TrackerRecordCoreData.fetchRequest()
-        let trackerRecordFromCoreData = try context.fetch(request)
+extension TrackerRecordStore: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        insertedIndexes = IndexSet()
+        deletedIndexes = IndexSet()
+        updatedIndexes = IndexSet()
+        movedIndexes = Set<TrackerRecordStoreUpdate.Move>()
+    }
 
-        return try trackerRecordFromCoreData.map { try self.trackerRecord(from: $0) }
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        guard
+            let insertedIndexes,
+            let deletedIndexes,
+            let updatedIndexes,
+            let movedIndexes
+        else { return }
+
+        delegate?.store(
+            self,
+            didUpdate: TrackerRecordStoreUpdate(
+                insertedIndexes: insertedIndexes,
+                deletedIndexes: deletedIndexes,
+                updatedIndexes: updatedIndexes,
+                movedIndexes: movedIndexes
+            )
+        )
+        self.insertedIndexes = nil
+        self.deletedIndexes = nil
+        self.updatedIndexes = nil
+        self.movedIndexes = nil
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .delete:
+            guard let indexPath else { return }
+            deletedIndexes?.insert(indexPath.item)
+        case.insert:
+            guard let indexPath = newIndexPath else { return }
+            insertedIndexes?.insert(indexPath.item)
+        case .update:
+            guard let indexPath else { return }
+            updatedIndexes?.insert(indexPath.item)
+        case .move:
+            guard let oldIndexPath = indexPath, let newIndexPath = newIndexPath else { return }
+            movedIndexes?.insert(.init(oldIndex: oldIndexPath.item, newIndex: newIndexPath.item))
+        default:
+            break
+        }
     }
 }
